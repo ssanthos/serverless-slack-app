f3141e2cf7382f7dc68ea17d4031a571ec825373
Srijith <srijiths89@gmail.com>
2018-08-02 21:11:34 +0530

    Got bot to respond on a mention

--- src/index.js
+++ src/index.js
@@ -47,9 +47,60 @@ slack.on('/register', (msg, bot) => {
     bot.replyPrivate(message);
 })
 
+async function askToRegister() {
+
+}
+
+function isInStr(searchKeys, searchStr) {
+    return !!searchKeys.find(key => searchStr.includes(key))
+}
+
+// mention event
+slack.on('app_mention', async (payload, bot) => {
+
+    try {
+        console.log(JSON.stringify(payload, null, 2));
+
+        const { user, type, text } = payload.event
+
+        if (type === "app_mention") {
+
+            const saidHi = isInStr(['hey', 'hi', 'hello'], text.toLowerCase())
+
+            if (saidHi) {
+
+                // const data = await slack.store.get(`user_${user}`)
+                //
+                // if (data) {
+                //
+                // }
+
+                await bot.reply({
+                    text: ':wave: You called on me?',
+                    // attachments: [{
+                    //   fallback: 'actions',
+                    //   callback_id: "actions_click",
+                    //   actions: [
+                    //     { type: "button", name: "Register", text: ":wave:", value: ":wave:" },
+                    //     { type: "button", name: "AddProgress", text: "Hiya", value: "Hiya" }
+                    //   ]
+                    // }]
+                })
+            }
+        }
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+slack.on('messages.channel', (payload, bot) => {
+
+})
+
 
 // Interactive Message handler
-slack.on('greetings_click', (msg, bot) => {
+slack.on('actions_click', (msg, bot) => {
+    console.log(JSON.stringify(msg, null, 2));
   let message = {
     // selected button value
     text: msg.actions[0].value
