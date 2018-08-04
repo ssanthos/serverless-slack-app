eeab7287d31f51ddc8f9f63266dfb95b5b045c1c
Srijith <srijiths89@gmail.com>
2018-08-04 22:16:19 +0530

    - Added serverless-offline plugin to faster development - Added help options which shows interactive buttons

--- src/index.js
+++ src/index.js
@@ -47,33 +47,36 @@ slack.on('/register', (msg, bot) => {
     bot.replyPrivate(message);
 })
 
-async function askToRegister() {
-
-}
-
 function isInStr(searchKeys, searchStr) {
-    return !!searchKeys.find(key => searchStr.includes(key))
+    if (typeof searchKeys === 'string') {
+        searchKeys = [ searchKeys ]
+    }
+    const words = searchStr.split(' ').map(v => v.trim().toLowerCase()).filter(v => !!v)
+    return !!searchKeys.find(key => !!words.find(w => w === key))
 }
 
 // mention event
 slack.on('app_mention', async (payload, bot) => {
 
     try {
+        console.log('app_mention');
         console.log(JSON.stringify(payload, null, 2));
 
         const { user, type, text } = payload.event
 
         if (type === "app_mention") {
 
-            const saidHi = isInStr(['hey', 'hi', 'hello'], text.toLowerCase())
+            const saidHi = isInStr(['hey', 'hi', 'hello'], text)
 
             if (saidHi) {
 
-                // const data = await slack.store.get(`user_${user}`)
-                //
-                // if (data) {
-                //
-                // }
+                const data = await slack.store.get(`user_${user}`)
+
+                if (data) {
+                    console.log('data', JSON.stringify(data));
+                } else {
+                    console.log('NO data');
+                }
 
                 await bot.reply({
                     text: ':wave: You called on me?',
@@ -93,21 +96,96 @@ slack.on('app_mention', async (payload, bot) => {
     }
 })
 
-slack.on('messages.channel', (payload, bot) => {
+function identifyRequest(text) {
+    if (isInStr(['register'], text)) {
+        return 'register'
+    } else if (isInStr(['update', 'updates', 'progress'], text)) {
+        return 'update'
+    } else if (isInStr(['help'], text)) {
+        return 'help'
+    } else if (isInStr(['pot'], text)) {
+        return 'pot'
+    } else if (isInStr(['account', 'balance'], text)) {
+        return 'balance'
+    }
+}
 
+// Messages
+slack.on('message', (payload, bot) => {
+    try {
+        console.log('message');
+        console.log(JSON.stringify(payload, null, 2));
+
+        const { channel_type, user, text } = payload.event
+        if (channel_type === 'im') {
+            // Direct message
+
+            if (identifyRequest(text) === 'help') {
+
+                bot.say({
+                    text : `I'm here to help`,
+                    attachments: JSON.stringify([
+                        {
+                            "text": "What do you wanna do?",
+                            "fallback": "You are unable to make a choice",
+                            "callback_id": "help_action_click",
+                            "color": "#3AA3E3",
+                            "attachment_type": "default",
+                            "actions": [
+                                {
+                                    "name": "help",
+                                    "text": "Register",
+                                    "type": "button",
+                                    "value": "register"
+                                },
+                                {
+                                    "name": "help",
+                                    "text": "Peek into the pot",
+                                    "type": "button",
+                                    "value": "check_pot"
+                                },
+                                {
+                                    "name": "help",
+                                    "text": "Post my progress",
+                                    "type": "button",
+                                    "value": "post_progress",
+                                },
+                            ]
+                        }
+                    ])
+                })
+
+            } else {
+                // Check for trigger words
+                bot.say({
+                    text : `I don't have a clue about what you just said. Ask me about things I know - like help`
+                })
+            }
+        } else if (channel_type === 'channel') {
+            // bot.say({
+            //     text : `Did you ask me this? - _${text}_`
+            // })
+        }
+    } catch (err) {
+        console.error(err);
+    }
 })
 
 
 // Interactive Message handler
-slack.on('actions_click', (msg, bot) => {
-    console.log(JSON.stringify(msg, null, 2));
-  let message = {
-    // selected button value
-    text: msg.actions[0].value
-  };
-
-  // public reply
-  bot.reply(message);
+slack.on('help_action_click', (msg, bot) => {
+    try {
+        console.log(JSON.stringify(msg, null, 2));
+    } catch (err) {
+        console.log(err);
+    }
+  // let message = {
+  //   // selected button value
+  //   text: msg.actions[0].value
+  // };
+  //
+  // // public reply
+  // bot.reply(message);
 });
 
 
